a0b5cac4ec4e53776447df0c49f4943bd2a238a5
Rafael Ortolani <rafael.ortolani@gmail.com>
2020-06-20 08:30:40 -0300

    inicio star-wars

---
+++ src/apis/star-wars.api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const baseURL = process.env.REACT_APP_STAR_WARS_BASE_URL;
+
+export const getFilms = () => {
+    return axios.request({ baseURL, url: 'films' })
+  }
+  
+  export const getFilmById = (id: number) => {
+    return axios.request({ baseURL, url: `films/${id}` })
+  }
