271c14e8c9ed254f3df739e8c8eadb998d8d4d91
Rafael Ortolani <rafael.ortolani@gmail.com>
2020-06-20 11:09:44 -0300

    correções

--- src/mobx/index.ts
+++ src/mobx/index.ts
@@ -2,10 +2,12 @@ import { home } from '../containers/home/store';
 import { router } from './router.store';
 import { combustivel } from '../containers/combustivel/store';
 import { starWars } from '../containers/star-wars/store';
+import { starWarsDetails } from '../containers/star-wars-details/store';
 
 export {
   router,
   home,
   combustivel,
-  starWars
+  starWars,
+  starWarsDetails,
 }
