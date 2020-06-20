136107f1b1daa4c16530e123b629fbf783643ea2
Rafael Ortolani <rafael.ortolani@gmail.com>
2020-06-20 11:03:47 -0300

    details

--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -3,7 +3,7 @@ import Sobre from '../containers/sobre';
 import Home from '../containers/home';
 import Combustivel from '../containers/combustivel';
 import StarWars from '../containers/star-wars'
-
+import StarWarsDetails from '../containers/star-wars-details'
 const publicUrl = process.env.PUBLIC_URL;
 
 interface EndPointsProps extends RouteProps {
@@ -15,5 +15,6 @@ export const endpoints: EndPointsProps[] = [
   { path: `${publicUrl}/home`, name: 'Home', component: Home, exact: true },
   { path: `${publicUrl}/combustivel`, name: 'Combustivel', component: Combustivel, exact: true },
   { path: `${publicUrl}/star-wars`, name: 'Star Wars', component: StarWars, exact: true },
+  { path: `${publicUrl}/star-wars/:id`, component: StarWarsDetails, exact: true },
   { path: `${publicUrl}/sobre`, name: 'Sobre', component: Sobre, exact: true },
 ];
