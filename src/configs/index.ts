c2a4cab2ddb8e5c43250e5544cee4fa8e876b083
Rafael Ortolani <rafael.ortolani@gmail.com>
2020-07-04 11:53:03 -0300

    github

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,7 +2,8 @@ export const configs= {
     apis: {
         economia: process.env.REACT_APP_ECONOMINA_URL,
         starWars: process.env.REACT_APP_STAR_WARS_BASE_URL,
-        correios: process.env.REACT_APP_CEP_URL
+        correios: process.env.REACT_APP_CEP_URL,
+        github: process.env.REACT_APP_GITHUB_URL
     },
     sentry: process.env.REACT_APP_SENTRY_DNS,
     onesignal: process.env.REACT_APP_ONE_SIGNAL!,
